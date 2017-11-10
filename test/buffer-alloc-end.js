9737452286cdac3cc44614dc091045b1b676b891
mvcbox <mvcbox.org@gmail.com>
2017-11-10 02:53:29 +0200

    Add tests

---
+++ test/buffer-alloc-end.js
@@ -0,0 +1,28 @@
+const expect  = require('chai').expect;
+const ExtendedBuffer = require('../src/extended-buffer');
+
+describe('buffer.allocEnd()', function () {
+    it('Alloc 1 byte', function() {
+        let buffer  = new ExtendedBuffer({
+            maxBufferLength: 10
+        });
+        buffer.allocEnd(1);
+        expect(buffer.getFreeSpaceEnd()).to.equal(5);
+    });
+
+    it('Alloc 5 bytes', function() {
+        let buffer  = new ExtendedBuffer({
+            maxBufferLength: 10
+        });
+        buffer.allocEnd(5);
+        expect(buffer.getFreeSpaceEnd()).to.equal(5);
+    });
+
+    it('Alloc 10 bytes', function() {
+        let buffer  = new ExtendedBuffer({
+            maxBufferLength: 10
+        });
+        buffer.allocEnd(10);
+        expect(buffer.getFreeSpaceEnd()).to.equal(10);
+    });
+});
---
+++ test/buffer-alloc-start.js
@@ -0,0 +1,28 @@
+const expect  = require('chai').expect;
+const ExtendedBuffer = require('../src/extended-buffer');
+
+describe('buffer.allocStart()', function () {
+    it('Alloc 1 byte', function() {
+        let buffer  = new ExtendedBuffer({
+            maxBufferLength: 10
+        });
+        buffer.allocStart(1);
+        expect(buffer.getFreeSpaceStart()).to.equal(5);
+    });
+
+    it('Alloc 5 bytes', function() {
+        let buffer  = new ExtendedBuffer({
+            maxBufferLength: 10
+        });
+        buffer.allocStart(5);
+        expect(buffer.getFreeSpaceStart()).to.equal(5);
+    });
+
+    it('Alloc 10 bytes', function() {
+        let buffer  = new ExtendedBuffer({
+            maxBufferLength: 10
+        });
+        buffer.allocStart(10);
+        expect(buffer.getFreeSpaceStart()).to.equal(10);
+    });
+});
