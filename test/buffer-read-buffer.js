9c920fcedb676bc070abb0adc9270671f61b12ad
mvcbox <mvcbox.org@gmail.com>
2017-11-11 02:55:48 +0200

    Make tests and update readBuffer()

--- test/buffer-read-buffer.js
+++ test/buffer-read-buffer.js
@@ -16,25 +16,13 @@ describe('buffer.readBuffer()', function () {
 
     it('Test #3', function() {
         let buf1 = (new ExtendedBuffer).writeBuffer(Buffer.from([1, 2, 3, 4, 5]));
-        let buf2 = Buffer.from([1, 2, 3, 4, 5]);
-        expect(Buffer.compare(buf1.readBuffer().buffer, buf2)).to.equal(0);
-    });
-
-    it('Test #4', function() {
-        let buf1 = (new ExtendedBuffer).writeBuffer(Buffer.from([1, 2, 3, 4, 5]));
         let buf2 = Buffer.from([1, 2, 3]);
         expect(Buffer.compare(buf1.readBuffer(3, true), buf2)).to.equal(0);
     });
 
-    it('Test #5', function() {
+    it('Test #4', function() {
         let buf1 = (new ExtendedBuffer).writeBuffer(Buffer.from([1, 2, 3, 4, 5])).offset(3);
         let buf2 = Buffer.from([4, 5]);
         expect(Buffer.compare(buf1.readBuffer(2, true), buf2)).to.equal(0);
-    });
-
-    it('Test #5', function() {
-        let buf1 = (new ExtendedBuffer).writeBuffer(Buffer.from([1, 2, 3, 4, 5]));
-        let buf2 = Buffer.from([1, 2, 3, 4, 5]);
-        expect(Buffer.compare(buf1.readBuffer(undefined, true), buf2)).to.equal(0);
     });
 });
