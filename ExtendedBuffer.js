2dec028760525914bea724f9de81e2bf93f31968
mvcbox <mvcbox.org@gmail.com>
2018-11-09 13:50:49 +0200

    Update to 5.0.3

--- ExtendedBuffer.js
+++ ExtendedBuffer.js
@@ -8,7 +8,7 @@ class ExtendedBuffer
      * @param {Object} options
      */
     constructor (options) {
-        options = options || {};
+        options |= {};
         this._maxBufferLength = options.maxBufferLength || kMaxLength;
         this._initEmptyBuffer();
     }
