5179964a7954ec02f3541eedb1442df1778aae09
mvcbox <mvcbox.org@gmail.com>
2026-01-31 05:14:48 +0200

    Update utils

--- src/utils/alloc-native-buffer.ts
+++ src/utils/alloc-native-buffer.ts
@@ -2,7 +2,33 @@ import { Buffer, kMaxLength } from 'buffer';
 import { ExtendedBufferRangeError } from '../errors';
 import { assertUnsignedInteger } from './assert-unsigned-integer';
 
-const maxBufferSize =
+const totalMem: number | undefined = (function() {
+  try {
+    const req: ((id: string) => unknown) | undefined =
+      typeof module !== 'undefined' && typeof module.require === 'function'
+        ? module.require.bind(module)
+        : typeof require === 'function'
+          ? require
+          : undefined;
+
+    if (!req) {
+      return undefined;
+    }
+
+    const os = req('os') as { totalmem?: () => number } | undefined;
+
+    if (typeof os?.totalmem !== 'function') {
+      return undefined;
+    }
+
+    const result = os.totalmem();
+    return Number.isSafeInteger(result) && result > 0 ? result : undefined;
+  } catch {
+    return undefined;
+  }
+})();
+
+const fallbackMaxLength =
   typeof kMaxLength === 'number'
     ? kMaxLength
     : typeof (Buffer as any).kMaxLength === 'number'
@@ -10,6 +36,8 @@ const maxBufferSize =
       : typeof (Buffer as any).constants?.MAX_LENGTH === 'number'
         ? (Buffer as any).constants.MAX_LENGTH
         : Number.MAX_SAFE_INTEGER;
+
+const maxBufferSize = typeof totalMem === 'number' ? Math.min(fallbackMaxLength, totalMem) : fallbackMaxLength;
 
 assertUnsignedInteger(maxBufferSize);
 
