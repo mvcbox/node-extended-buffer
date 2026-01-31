02032e6c996468af514315577fbb0d5ab8065bd6
mvcbox <mvcbox.org@gmail.com>
2026-01-31 04:57:25 +0200

    Update utils

--- src/utils/assert-support-big-integer.ts
+++ src/utils/assert-support-big-integer.ts
@@ -1,14 +1,23 @@
 import { Buffer } from 'buffer';
 import { ExtendedBufferUnsupportedError } from '../errors';
 
+const requiredMethods = [
+  'readBigUInt64LE',
+  'readBigUInt64BE',
+  'readBigInt64LE',
+  'readBigInt64BE',
+  'writeBigUInt64LE',
+  'writeBigUInt64BE',
+  'writeBigInt64LE',
+  'writeBigInt64BE'
+] as const;
+
+const isSupported =
+  typeof BigInt === 'function' &&
+  requiredMethods.every(method => typeof (Buffer.prototype as any)[method] === 'function');
+
 export function assertSupportBigInteger(): void {
-  let isSupported = false;
-
-  try {
-    isSupported = typeof BigInt(0) === 'bigint';
-  } catch (e) {}
-
-  if (!isSupported || typeof Buffer.prototype.readBigUInt64LE !== 'function') {
+  if (!isSupported) {
     throw new ExtendedBufferUnsupportedError('EXECUTION_ENVIRONMENT_NOT_SUPPORT_BIG_INT');
   }
 }
